8e64198abd29b067bae86c8488042275729a9866
Katrien Monteyne <katrien.monteyne@outlook.com>
2017-11-13 10:22:56 +0100

    Profile databinding

--- Dare'm/app/src/main/java/be/nmct/howest/darem/ProfileActivity.java
+++ Dare'm/app/src/main/java/be/nmct/howest/darem/ProfileActivity.java
@@ -1,7 +1,10 @@
 package be.nmct.howest.darem;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import be.nmct.howest.darem.databinding.ActivityProfileBinding;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ActivityProfileBinding activityProfileBinding = DataBindingUtil.setContentView(this, R.layout.activity_profile);
+
     }
 }
